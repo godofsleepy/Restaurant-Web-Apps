154301fa5a14edefe8e11f2c2a1ffe2f13131a2b
Rifat Khadafy <45101689+godofsleepy@users.noreply.github.com>
2021-07-23 21:56:20 +0700

    cache app shell

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,6 +2,7 @@ const CONFIG = {
   KEY: 'ac80477002d21dc4400901f64c0506a7',
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
+  CACHE_NAME: 'Resturant-apps',
 };
 
 export default CONFIG;
